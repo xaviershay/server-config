633dc4149cfb63aa9760226889a2f1ea06c10694
Xavier Shay <xavier@rhnh.net>
2024-12-31 14:36:15 +1100

    WIP

---
+++ ruby/scripts/install_influxdb.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# From https://docs.influxdata.com/influxdb/v2/install/?t=Linux
+set -ex
+
+curl --silent --location -O https://repos.influxdata.com/influxdata-archive.key
+echo "943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515  influxdata-archive.key" \
+| sha256sum --check -
+
+cat influxdata-archive.key \
+| gpg --dearmor \
+| tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null \
+
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' \
+| tee /etc/apt/sources.list.d/influxdata.list
+
+# Install influxdb
+sudo apt-get update
+sudo apt-get install influxdb2 -y
---
+++ ruby/styx.rb
@@ -0,0 +1,122 @@
+require 'set'
+
+class Babs
+  # Progress until a met? is not met then halt
+  def plan
+    completed = Set.new
+    puts @@root.inspect
+    @root.each do |task_name|
+    end
+  end
+
+  # Progress fully
+  def apply
+  end
+
+  def self.root_task(deps)
+    @root = deps
+  end
+   def self.task(name, depends: [])
+end
+
+class Styx < Babs
+  task 'influxdb: install' do
+    met? { run("influxd version").start_with?("InfluxDB v2.7.11") }
+    meet {
+      run_script("install_influxdb.bash")
+    }
+  end
+
+  task 'influxdb: run' do
+    met? { false } # TODO: Copy health check from ansible
+    meet {
+      run("sudo systemctl restart influxdb")
+    }
+  end
+
+  sftp_task 'influxdb: configure', [
+    '/etc/influxdb2/config.yml'
+  ]
+
+  task 'influxdb', depends: [
+    'influxdb: install',
+    'influxdb: configure',
+    'influxdb: run'
+  ]
+
+  %w(system sensors).each do |bucket_name|
+    task "ensure influx bucket: #{bucket_name}", depends: 'influxdb' do
+      met? {
+        buckets = JSON.parse(run("influx bucket list --json"))
+        bucket = buckets.find {|x| x.fetch('name') == name }
+        if bucket
+          store_variable "influxdb.buckets.#{bucket_name}", bucket.fetch('id')
+        end
+      }
+      meet {
+        run("influx bucket create --name %s" % [
+          name,
+        ])
+      }
+    end
+  end
+
+  {
+    grafana: {read: ['system', 'sensor']},
+    telegraf: {write: ['system']}
+  }.each do |description, permissions|
+    task "ensure influxdb token: #{description}", depends: 'influxdb' do
+      met? {
+        tokens = JSON.parse(run("influx auth list --json"))
+        token = tokens.find {|x| x.fetch('description') == description }
+        if token
+          store_variable 'telegraf.influxdb.api_token', token.fetch('token')
+        end
+      }
+      meet {
+        flags = ""
+        flags << permissions.fetch(:read, []).map {|x|
+          "--read-bucket %s" % read_variable "influxdb.buckets.#{x}"
+        }
+        flags << permissions.fetch(:write, []).map {|x|
+          "--write-bucket" % read_variable "influxdb.buckets.#{x}"
+        }
+        run("influx auth create --description \"%s\" %s" % [
+          description,
+          flags
+        ])
+      }
+    end
+  end
+
+  task 'telegraf: install' do
+    met? { false } # TODO run("influxd version").start_with?("InfluxDB v2.7.11") }
+    meet {
+      run_script("install_telegraf.bash")
+    }
+  end
+
+  sftp_task 'telegraf: configure', [
+    '/etc/telegraf/telegraf.conf'
+  ], depends: 'ensure telegraf token'
+
+  task 'telegraf: run' do
+    met? { false } # TODO: Copy health check from ansible
+    meet {
+      run("sudo systemctl restart telegraf")
+    }
+  end
+
+  task 'telegraf', depends: [
+    'telegraf: install',
+    'telegraf: configure',
+    'telegraf: run'
+  ]
+
+  root_task [
+    'influxdb',
+    'telegraf'
+  ]
+end
+
+Styx.new.plan
