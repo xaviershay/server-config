b296eb7aec8d43a329f69e7f4c56968b8d316a0d
Xavier Shay <contact@xaviershay.com>
2025-01-03 10:32:38 +1100

    Add blocky

--- ruby/styx.rb
+++ ruby/styx.rb
@@ -163,6 +163,35 @@ class Styx < Babs
     'grafana: enable'
   ]
 
+  task 'blocky: install' do
+    met? { run("blocky version | grep Version || true").start_with?("Version: v0.24") }
+    meet {
+      run_script("install_blocky.bash")
+    }
+  end
+
+  sftp_task 'blocky: configure', [
+    '/etc/blocky/config.yml',
+    '/etc/systemd/system/blocky.service'
+  ], 644
+
+  task 'blocky: run' do
+    met? { run("systemctl is-active --quiet blocky && echo OK").start_with?("OK") }
+    meet {
+      run("sudo systemctl restart blocky")
+    }
+  end
+
+  task 'blocky: enable', &systemctl_enable_task('blocky')
+
+  task 'blocky', depends: [
+    'blocky: install',
+    'blocky: configure',
+    'blocky: run',
+    'blocky: enable'
+  ]
+
+
   task 'hostname' do
     met? {
       @name = read_variable 'hostname'
@@ -203,7 +232,8 @@ class Styx < Babs
     'system',
     'influxdb',
     'telegraf',
-    'grafana'
+    'grafana',
+    'blocky',
   ]
 end
 
