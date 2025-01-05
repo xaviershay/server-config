fa90793bde4cec7e68666a2671ad04372cf6480d
Xavier Shay <contact@xaviershay.com>
2025-01-06 09:56:23 +1100

    Initial cloudflare DNS import from DNSimple

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
   }
 
   backend "s3" {
