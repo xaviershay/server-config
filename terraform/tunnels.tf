fd7742a816d81a1ebb702194c02519d7e633a2f0
Xavier Shay <contact@xaviershay.com>
2025-01-20 17:08:54 +1100

    cloudflared tunnel config

---
+++ terraform/tunnels.tf
@@ -0,0 +1,80 @@
+resource "random_id" "tunnel_secret" {
+  byte_length = 64
+}
+
+variable "allowed_users" {
+  type = list(string)
+}
+
+output "cloudflared_tunnel_id" {
+  description = "ID of the created tunnel"
+  value       = cloudflare_zero_trust_tunnel_cloudflared.auto_tunnel.tunnel_token
+  sensitive   = true
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "auto_tunnel" {
+  account_id = var.cloudflare_account_id
+  name       = "Home tunnel"
+  secret     = base64sha256(random_id.tunnel_secret.b64_std)
+}
+
+resource "cloudflare_record" "http_app" {
+  zone_id = module.zone_xaviershay_com.id
+  name    = "home"
+  content   = "${cloudflare_zero_trust_tunnel_cloudflared.auto_tunnel.cname}"
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_zero_trust_access_application" "http_app" {
+  zone_id = module.zone_xaviershay_com.id
+  name             = "Home"
+  domain           = "home.xaviershay.com"
+  session_duration = "24h"
+}
+
+# Creates the configuration for the tunnel.
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "auto_tunnel" {
+  tunnel_id = cloudflare_zero_trust_tunnel_cloudflared.auto_tunnel.id
+  account_id = var.cloudflare_account_id
+  config {
+   ingress_rule {
+     hostname = "${cloudflare_record.http_app.hostname}"
+     service  = "http://localhost:80"
+     origin_request {
+       connect_timeout = "2m0s"
+       access {
+         required  = true
+         team_name = "xaviershay"
+         aud_tag   = [cloudflare_zero_trust_access_application.http_app.aud]
+       }
+     }
+   }
+   ingress_rule {
+     service  = "http_status:404"
+   }
+  }
+}
+
+# Create an Access Group for the single user
+resource "cloudflare_access_group" "home" {
+  account_id = var.cloudflare_account_id
+  name       = "home"
+
+  include {
+    email = var.allowed_users
+  }
+}
+
+# Create an Access Policy and attach it to the HTTP application
+resource "cloudflare_access_policy" "allow_user" {
+  account_id     = var.cloudflare_account_id
+  application_id = cloudflare_zero_trust_access_application.http_app.id
+  name           = "Home Access"
+  precedence     = 1
+  decision       = "allow"
+
+  include {
+    group = [cloudflare_access_group.home.id]
+  }
+}
